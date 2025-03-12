1dab93df1cb4c9bbace61abf888fa9864d6a0cd6
Joshua Atanu <atanu.joshua@gmail.com>
2025-03-12 07:23:17 +0000

    Update splash screen icon and background color; change translation API endpoint

--- app/translate.tsx
+++ app/translate.tsx
@@ -32,7 +32,7 @@ export default function TranslatePage() {
   const handleTranslate = async () => {
     try {
       setLoading(true);
-      const response = await fetch("http://10.130.137.16:5000/translate", {
+      const response = await fetch("https://api-ex6d.onrender.com/translate", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ "english-text": inputText }),
